40bff96cf459b9683b339f83c4a20c7608609efe
hernang87 <hernang@mural.co>
2024-01-03 13:11:07 -0300

    Initial config

---
+++ nodejs/src/app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const logger = require('morgan');
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(logger('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => {
+  console.info(`App listening on port ${PORT}`);
+});
