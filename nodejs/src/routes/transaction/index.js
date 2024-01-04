2970737c353b1b615c32216cff401aa42efbaf1a
hernang87 <hernang@mural.co>
2024-01-03 21:30:19 -0300

    Remove duplicated files

--- nodejs/src/routes/transaction/index.js
+++ nodejs/src/routes/transaction/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const process = require("./process");
-const validate = require("./validate");
+const process = require("./process-transaction");
+const validate = require("./validate-transaction");
 
 router.post("/", validate, process);
 
